19ac5e300afe9ff5e95d7e79154df81fa2a214ff
leoyong <y_c_l__@live.com>
2016-11-06 23:56:57 +0800

    test code

---
+++ test_code/Receiver.h
@@ -0,0 +1,32 @@
+//Auth: Leo Yong
+
+//Receive the signal from receiver and process the direction and generate driving pwm.
+
+#define timeOut  25000
+#define timeUpdate  100
+
+//=====BEGIN Channel CLASS=====
+class Channel {
+
+  public:
+  int recPin,sigIn,sigOut,dirL,dirR;
+  int prevTime;  
+  Channel (int rec) : recPin(rec) {
+    pinMode (recPin, INPUT);
+    Serial.begin(9600); // Pour a bowl of Serial
+
+    }
+
+   int sigRead(unsigned long currentTime) {
+    if (currentTime - prevTime >= timeUpdate){
+      sigIn = pulseIn(recPin,HIGH,timeOut);   Serial.println(sigIn);
+      prevTime = currentTime;
+    }
+    return sigIn;
+   }
+   int sig2Motor(int sig) {
+     sigOut = map(sig, 1000,2000,-500,500); return sigOut;
+   }
+  int dir2Motor () {}
+};
+//=====END CLASS======
---
+++ test_code/test_code.ino
@@ -0,0 +1,32 @@
+/*
+Auth: Leo Yong
+
+
+*/
+ #include "Receiver.h"
+
+Channel *ch1;
+Channel *ch2;
+
+int sigIn1, sigIn2,count;
+
+void setup() {
+  
+  ch1 = new Channel(5);
+  ch2 = new Channel(6);
+  
+
+  OCR0A = 0xAF;
+  TIMSK0 |= _BV(OCIE0A);
+}
+
+  SIGNAL(TIMER0_COMPA_vect)
+{
+  unsigned long currentMillis = millis();
+  
+   sigIn1 = ch1->sigRead(currentMillis);
+   sigIn2 = ch2->sigRead(currentMillis);
+ 
+}
+void loop() { 
+  }
